5fea33924aee2d985951b0b59e99fe00eeda9b69
sanjvj <sanjaysurya3010@gmail.com>
2024-08-16 21:05:54 +0530

    Slider animation

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -1,20 +1,64 @@
 import React from "react";
 import "./Slider.css";
+
 const Slider = () => {
   return (
     <div>
       <div className="logos">
         <div className="logos-slide-wrapper">
           <div className="logos-slide">
-            <h1>Affordable</h1>
-            <h1>Daily made Products</h1>
-            <h1>No chemical</h1>
+            {/* Flex container for image and text */}
+            <div className="flex items-center">
+              <img
+                src="Affordable.svg"
+                className="h-5 w-5 "
+                alt="Affordable Icon"
+              />
+              <h1 className="ml-0">Affordable</h1>
+            </div>
+            <div className="flex items-center">
+              <img
+                src="Dailymade.svg"
+                className="h-5 w-5 "
+                alt="Affordable Icon"
+              />
+              <h1 className="ml-0">Daily Made</h1>
+            </div>
+            <div className="flex items-center">
+              <img
+                src="Nochemical.svg"
+                className="h-5 w-5 "
+                alt="Affordable Icon"
+              />
+              <h1 className="ml-0">No chemicals</h1>
+            </div>
           </div>
 
           <div className="logos-slide">
-            <h1>Affordable</h1>
-            <h1>Daily made Products</h1>
-            <h1>No chemical</h1>
+            <div className="flex items-center">
+              <img
+                src="Affordable.svg"
+                className="h-5 w-5 "
+                alt="Affordable Icon"
+              />
+              <h1 className="ml-0">Affordable</h1>
+            </div>
+            <div className="flex items-center">
+              <img
+                src="Dailymade.svg"
+                className="h-5 w-5 "
+                alt="Affordable Icon"
+              />
+              <h1 className="ml-0">Daily Made</h1>
+            </div>
+            <div className="flex items-center">
+              <img
+                src="Nochemical.svg"
+                className="h-5 w-5 "
+                alt="Affordable Icon"
+              />
+              <h1 className="ml-0">No chemicals</h1>
+            </div>
           </div>
         </div>
       </div>
