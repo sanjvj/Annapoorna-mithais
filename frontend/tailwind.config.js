d53aae0d5b8850f668c4698bdb0cab00358eb033
sanjvj <sanjaysurya3010@gmail.com>
2024-08-30 13:46:46 +0530

    Overlay design animated and updated for mobile

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,19 +4,23 @@ module.exports = {
     extend: {
       fontFamily: {
         Nunito: ['"Nunito Sans"', 'serif'],
-        Philosopher : ['"Philosopher"','serif']
-      },keyframes: {
-        slideUp: {
+        Philosopher: ['"Philosopher"', 'serif'],
+      },
+      animation: {
+        slide: 'slide 35s linear infinite',
+        slideUp: 'slideUp 0.5s ease-out', // New slide-up animation
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        slideUp: { // New keyframes for slide-up
           '0%': { transform: 'translateY(100%)' },
           '100%': { transform: 'translateY(0)' },
         },
       },
-      animation: {
-        slideUp: 'slideUp 0.3s ease-out',
-      },
     },
   },
-  plugins: [
-    
-  ],
+  plugins: [],
 }
