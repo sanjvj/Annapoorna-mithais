705ef82482c794fae1a521c1b78b6932da784751
sanjvj <sanjaysurya3010@gmail.com>
2024-09-01 14:25:54 +0530

    Filter added

--- frontend/src/pages/ShopNow/ShopNow.jsx
+++ frontend/src/pages/ShopNow/ShopNow.jsx
@@ -7,6 +7,7 @@ import ShopHero from '../../components/ShopHero';
 import Footer from '../../components/Footer';
 import SearchBar from '../../components/SearchBar';
 import SearchWithFilter from '../../components/SearchWithFilter';
+import MenuFilter from '../../components/MenuFilter';
 
 const ShopNow = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -17,6 +18,8 @@ const ShopNow = () => {
       <Slider />
       <ExploreMenu title1='What are you' title2='searching for?' />
       <SearchWithFilter searchTerm={searchTerm} setSearchTerm={setSearchTerm}></SearchWithFilter>
+      <MenuFilter></MenuFilter>
+      
       <FoodDisplay searchTerm={searchTerm} />
       <ShopHero />
       <Footer />
